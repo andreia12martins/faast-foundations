2eeb739c206b6453746234d804587d6fa3baec38
Andreia Martins <andreia.martins@nos.pt>
2024-04-07 20:13:05 +0100

    fixed tests - session scope to function scope

--- assignments/life_expectancy/tests/conftest.py
+++ assignments/life_expectancy/tests/conftest.py
@@ -5,19 +5,19 @@ import pytest
 from . import FIXTURES_DIR
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
-    return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv")
+    return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv", delimiter="\t")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def eu_life_expectancy_raw_subset() -> pd.DataFrame:
     """Fixture to load a raw subset of eu data"""
-    return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", sep="\t")
+    return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", delimiter="\t")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def eu_life_expectancy_expected_subset() -> pd.DataFrame:
     """Fixture to load the expected subset of cleaned eu data"""
-    return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_expected.csv", sep="\t")
+    return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_expected.csv", delimiter="\t")

a4fce614a636b9f8b70215d145aaefdfa1d3f969
Andreia Martins <andreia.martins@nos.pt>
2024-04-22 19:45:28 +0100

    Resolved scope in fixtures

--- assignments/life_expectancy/tests/conftest.py
+++ assignments/life_expectancy/tests/conftest.py
@@ -6,17 +6,17 @@ import pytest
 from . import FIXTURES_DIR
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
     return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def eu_life_expectancy_raw_subset() -> pd.DataFrame:
     return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", sep="\t")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def eu_life_expectancy_expected_subset() -> pd.DataFrame:
     return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_expected.csv", sep="\t")
